128b82c49790ad4fa2696693508a52d1e9b2902e
Mohammmed shahid <mhdshahid88@gmail.com>
2025-07-22 21:28:34 +0530

    notification

--- src/interface/middleware/auth.ts
+++ src/interface/middleware/auth.ts
@@ -10,6 +10,7 @@ interface JwtPayload {
 }
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   
